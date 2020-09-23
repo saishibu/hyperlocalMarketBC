34b6e4fb921c735c1b6f01001d5bd0f38aee1f0f
Sai Shibu N B <saishibu38380@gmail.com>
2020-09-23 14:26:30 +0530

    Update fuzzy.py

--- fuzzy.py
+++ fuzzy.py
@@ -82,6 +82,12 @@ def EVLoc(radius,noEV):
 		y = w * math.sin(t)
 		xLat  = x + x0
 		yLat = y + y0
+		
+		dlon = xLat - x0
+		dlat = yLat - y0
+		a = (sin(dlat/2))**2 + cos(lat1) * cos(lat2) * (sin(dlon/2))**2
+		c = 2 * atan2(sqrt(a), sqrt(1-a))
+		distance = R * c
 		soc=random.randint(41, 100)
-		loc.append([xLat,yLat,soc])
+		loc.append([xLat,yLat,distance,soc])
 	return loc
